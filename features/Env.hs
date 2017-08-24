689fae6f189e4b9e5f204e96bd4b03ea9b737a06
Felix Chapman <aelred717@gmail.com>
2017-08-24 11:46:13 +0100

    Fix `Env`

--- features/Env.hs
+++ features/Env.hs
@@ -2,33 +2,11 @@ module Env where
 
 import           Data.String.Utils (rstrip)
 import           System.Exit       (ExitCode (ExitSuccess))
-import           System.Process    (CreateProcess (CreateProcess),
-                                    createProcess, readCreateProcess,
-                                    waitForProcess)
+import           System.Process    (CreateProcess, createProcess, proc,
+                                    readCreateProcess, waitForProcess)
 
--- This is an inline of `System.Process.proc` because hfmt gets upset parsing
--- the word `proc` for some reason.
 env :: [String] -> CreateProcess
-env args =
-  CreateProcess
-  { cmdspec = RawCommand cmd args
-  , cwd = Nothing
-  , env = Nothing
-  , std_in = Inherit
-  , std_out = Inherit
-  , std_err = Inherit
-  , close_fds = False
-  , create_group = False
-  , delegate_ctlc = False
-  , detach_console = False
-  , create_new_console = False
-  , new_session = False
-  , child_group = Nothing
-  , child_user = Nothing
-  , use_process_jobs = False
-  }
-  where
-    cmd = "features/grakn-spec/env.sh"
+env = proc "features/grakn-spec/env.sh"
 
 envStart :: IO Bool
 envStart = envSuccess ["start", "0.16.0"]
@@ -54,6 +32,6 @@ envCheckInstance res value = envSuccess ["check", "instance", res, value]
 
 envSuccess :: [String] -> IO Bool
 envSuccess args = do
-  (_, _, _, p) <- createProcess $ env args
-  code <- waitForProcess p
-  return $ code == ExitSuccess
+    (_, _, _, p) <- createProcess $ env args
+    code <- waitForProcess p
+    return $ code == ExitSuccess
